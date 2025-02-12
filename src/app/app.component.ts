379797762f566c960b8e13f5f1afd55bc846efcb
Gavin Szczesniak <gavinszczesniak@gmail.com>
2025-02-11 19:09:17 -0500

    - Pushes init code - Makes initial pie chart of debits, credits, and balance. - Starts the styling of the amounts in numbers with soon to have percentage bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,58 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import { Component, NgZone } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Chart } from 'chart.js/auto';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CurrencyPipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'participant-ledger-18';
+  constructor(public NgZone: NgZone) {}
+
+  ngOnInit() {
+    this.createChart();
+  }
+
+  chart: any;
+
+  createChart() {
+    this.chart = new Chart('MyChart', {
+      type: 'doughnut',
+      data: {
+        labels: ['Debits', 'Credits', 'Balance'],
+        datasets: [{
+          label: 'Account Balance',
+          data: [100, 200, 300],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.5)',
+            'rgba(54, 162, 235, 0.5)',
+            'rgba(255, 206, 86, 0.5)'
+          ],
+          hoverOffset: 4,
+        }]
+      },
+      options: {
+        cutout: '85%',
+        maintainAspectRatio: false,
+        plugins: {
+          title: {
+            display: true,
+            text: 'Account Balance',
+            font: {
+              size: 20
+            },
+            padding: {
+              top: 10,
+              bottom: 10
+            },
+          },
+        }
+      }
+    });
+  }
+
 }
---
+++ src/main.single-spa.ts
@@ -0,0 +1,37 @@
+import { enableProdMode, NgZone } from '@angular/core';
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { getSingleSpaExtraProviders, singleSpaAngular } from 'single-spa-angular';
+
+import { AppComponent } from './app/app.component';
+import { environment } from './environments/environment.development';
+import { singleSpaPropsSubject } from './single-spa/single-spa-props';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import { EmptyRouteComponent } from './app/empty-route/empty-route.component';
+import { APP_BASE_HREF } from '@angular/common';
+import { Subject } from 'rxjs';
+
+// if (environment.production) {
+//   enableProdMode();
+// }
+console.log(window.location.pathname)
+
+const lifecycles = singleSpaAngular({
+  bootstrapFunction: singleSpaProps => {
+    singleSpaPropsSubject.next(singleSpaProps);
+    return bootstrapApplication(AppComponent, {
+      providers: [
+        getSingleSpaExtraProviders(),
+        { provide: APP_BASE_HREF, useValue: `${window.location.pathname}` },
+      ],
+    });
+  },
+  NgZone,
+  template: '<app-root></app-root>',
+});
+
+export const bootstrap = lifecycles.bootstrap;
+export const mount = lifecycles.mount;
+export const unmount = lifecycles.unmount;
